7423034876a3f1b67d1b3d3c6a6ea4f14febd7fe
Sajidur Rahman <sajibsrs@gmail.com>
2022-03-25 04:25:37 +0600

    updated to camelCase

--- src/array.go
+++ src/array.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	arr_one := [...]int{1, 2, 3}
-	var arr_two [3]string
+	arrOne := [...]int{1, 2, 3}
+	var arrTwo [3]string
 
-	arr_two[0] = "a"
-	arr_two[1] = "b"
-	arr_two[2] = "c"
+	arrTwo[0] = "a"
+	arrTwo[1] = "b"
+	arrTwo[2] = "c"
 
-	fmt.Println("arr_one:", arr_one)
-	fmt.Println("arr_two:", arr_two)
+	fmt.Println("arr_one:", arrOne)
+	fmt.Println("arr_two:", arrTwo)
 }
